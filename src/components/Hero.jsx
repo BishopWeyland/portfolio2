069cb3966f35b240693ed1129f8f96479907f850
Håkon Willand Engebretsen <willand27@hotmail.com>
2023-12-17 18:16:35 +0100

    change styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,14 @@ function Hero() {
           className="h-3/5 md:h-3/4 flex bg-cover bg-center text-left flex-col justify-end"
           style={{ backgroundImage: `url(${HeroImg})` }}
         >
-          <h1 className="pl-4 mb-4 text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold text-white">
+          <h1 className="pl-4 mb-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold text-white">
             Håkon
           </h1>
-          <h2 className="pl-4 mb-4 text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold  text-white ">
+          <h2 className="pl-4 mb-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold  text-white ">
             Willand
           </h2>
         </div>
-        <h2 className="pl-4 text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold text-black">
+        <h2 className="pl-4 text-5xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl font-lexend font-bold text-black">
           Engebretsen
         </h2>
         <div className="pl-4 mt-4">
